f04052b8a7ab823160dd09ad9ffc99344d13642a
alexomur <alexomur111@gmail.com>
2025-02-11 12:03:04 +0300

    Practice 1 init WIP

---
+++ Practice 1/DandyBot-main/random_bot.py
@@ -0,0 +1,8 @@
+﻿import random
+
+
+def script(check, x, y):
+    return "pass"
+    if check("gold", x, y):
+        return "take"
+    return random.choice(["pass", "left", "right", "up", "down"])
---
+++ Practice 1/DandyBot-main/user_bot.py
@@ -0,0 +1,25 @@
+﻿def draw_graph(check, x, y) -> None:
+    pass
+
+def level1(check, x, y):
+    if check("wall", x+2, y):
+        return "down"
+    return "right"
+
+def level2(check, x, y):
+    if check("wall", x, y-1):
+        return "right"
+    if (check("wall", x+2, y) or check("wall", x+1, y)) or check("wall", x, y-2):
+        return "up"
+    else:
+        return "right"
+
+def script(check, x, y):
+    if check("gold", x, y):
+        return "take"
+
+    if check("level") == 1:
+        return level1(check, x, y)
+    if check("level") == 2:
+        return level2(check, x, y)
+    return "pass"
