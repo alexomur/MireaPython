65394100da86cb11107ea00fbf6c4dc98daf1264
alexomur <alexomur111@gmail.com>
2025-02-13 23:22:51 +0300

    feat: AIArchive

--- Practice 1/DandyBot-main/random_bot.py
+++ Practice 1/DandyBot-main/random_bot.py
@@ -1,9 +1,9 @@
 ﻿import random
-import user_bot
+from AIArchive import v1
 
 
 def script(check, x, y):
-    return user_bot.script(check, x, y)
+    return v1.script(check, x, y)
     if check("gold", x, y):
         return "take"
     return random.choice(["pass", "left", "right", "up", "down"])
